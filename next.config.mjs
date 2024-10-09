96a2ed25bdafad38c953c1234418580d20bba6c0
boqian.liu <liuboqian2015@outlook.com>
2024-10-09 09:01:10 +0800

    add version number

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: "standalone",
+  generateBuildId: async () => {
+    return process.env.GIT_HASH;
+  },
   experimental: {
     instrumentationHook: true,
   },
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,8 +4,12 @@ import ExperiencePage from "./content/experience";
 import HelloPage from "./content/hello";
 import SkillPage from "./content/skill";
 import DownloadButton from "./download-button";
+import { fromUnixTime, parse } from "date-fns";
 
 export default function Home() {
+  const date = fromUnixTime(
+    parseInt(process.env.BUILD_AT ?? "")
+  ).toDateString();
   return (
     <main className="flex min-h-screen flex-col">
       <HelloPage />
@@ -14,6 +18,11 @@ export default function Home() {
       <Education />
       <Contact />
       <DownloadButton />
+      <footer className="text-sm text-gray-400 relative bottom-1 float-left">
+        Version: {process.env.VERSION}
+        <br />
+        Update At: {date}
+      </footer>
     </main>
   );
 }
