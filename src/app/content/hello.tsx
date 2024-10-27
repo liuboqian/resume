245f4b26cc1071ad72fb85dc78126f7ac694a9f8
boqian.liu <liuboqian2015@outlook.com>
2024-10-27 21:49:11 +0800

    update content

--- src/app/content/hello.tsx
+++ src/app/content/hello.tsx
@@ -8,7 +8,7 @@ export default function HelloPage() {
       id="hello"
       className="h-screen snap-start flex flex-col items-center justify-center gap-8 overflow-auto"
     >
-      <div className="flex flex-row">
+      <div className="flex-1 flex flex-row items-center">
         <Image
           src="/avatar.png"
           height={200}
@@ -17,7 +17,7 @@ export default function HelloPage() {
           className="m-auto mr-16"
         />
         <h1 className="text-6xl font-bold">
-          Hi,
+          <span className="text-8xl ">Hi</span>,
           <br />
           <span className="text-2xl md:text-4xl">
             This is <b>Boqian Liu (刘博谦)</b>
@@ -30,16 +30,19 @@ export default function HelloPage() {
       </div>
 
       <p className="text-xl m-4 mt-0">
-        I have worked 6 years of experience as a developer. First start from
+        7 years of experience as a developer. First start from
         <b> chemical engineering simulation</b> software, then switch to
         <b> health care software</b>.
         <br />
-        Now I&apos;m seeking for a senior developer role.
+        As a <b>full-stack developer</b>, I can contribute to the whole process
+        from system design to UI design, implementation and finally deliver to
+        customer.
         <br />
-        As a full-stack developer, I can contribute from system design to UI
-        design, implementation and finally deliver to customer.
+        As a <b>team player</b>, I value and cherish healthy relationships with
+        collages. Sometimes I organize tech talk to share my knowledge with team
+        members, and also get to know other territories from them.
       </p>
-      <p className="mt-8 text-center text-gray-400 animate-bounce">
+      <p className="my-8 text-center text-gray-400 animate-bounce">
         Slide for more...
       </p>
     </section>
